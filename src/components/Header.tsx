5d1a5695ce1a8fb6bc2df9738feb792023c9d2fa
lizhiyong <1679963578@qq.com>
2023-12-26 19:57:09 +0800

    fix ui

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -23,18 +23,25 @@ const Header: FC<{}> = ({}) => {
 
   return (
     <div>
-      <div className="  bg-[url(/headerBg.png)] h-20 flex items-center ">
-        <div className=" w-container mx-auto flex mo:w-full md:w-full md:px-[30px] mo:px-[30px]  justify-between  ">
+      <div className="  bg-[url(/headerBg.png)] border-opacity-50 h-20 flex items-center bg-cover border-b-[1px] border-[#FFFFFF] ">
+        <div className=" w-container  mx-auto flex mo:w-full md:w-full md:px-[30px] mo:px-[30px]    justify-between  ">
           <div className="  bg-[url(/logo.png)] bg-no-repeat w-full h-[26px] " />
-          <div className="  flex flex-row  items-center justify-between gap-10 md:mr-10 mo:mr-10 text-white">
-            <button>Bridge</button>
-            <button>Testnet</button>
-            <img src={"/x.png"} alt="" className="bg-no-repeat w-[16px] h-4" />
-            <img
-              src={"/brand.png"}
-              alt=""
-              className="bg-no-repeat w-[16px] h-4"
-            />
+          <div className="  flex flex-row  items-center justify-between gap-10  text-white">
+            <button
+              style={{ letterSpacing: "1.4px" }}
+              className=" text-sm font-medium "
+            >
+              Bridge
+            </button>
+            <button
+              style={{ letterSpacing: "1.4px" }}
+              className=" text-sm font-medium "
+            >
+              Testnet
+            </button>
+            <img src={"/x.png"} alt="" className="bg-no-repeat " />
+            <img src={"/brand.png"} alt="" className="bg-no-repeat" />
+            <button></button>
           </div>
         </div>
       </div>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
+      <link
+        href="https://fonts.googleapis.com/css?family=Outfit"
+        rel="stylesheet"
+      ></link>
+      <link rel="icon" href="/bitIcon.svg" />
       <Head />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
