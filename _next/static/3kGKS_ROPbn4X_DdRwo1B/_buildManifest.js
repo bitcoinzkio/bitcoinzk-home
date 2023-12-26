2232e4719690e1ee46fe419438ee4551c3e0f401
YongZL <YongZL@users.noreply.github.com>
2023-12-26 08:52:37 +0000

    deploy: 437bcd50ebb71db83c50db8014c5bd80574906e7

---
+++ _next/static/3kGKS_ROPbn4X_DdRwo1B/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/873-3fd481ecc1ff0962.js","static/chunks/pages/index-900c3d9fdf8cac02.js"],"/_error":["static/chunks/pages/_error-b6491f42fb2263bb.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
