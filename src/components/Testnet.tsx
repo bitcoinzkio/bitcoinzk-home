ceebc18e16608ae3bbc91c642a41b80f92cf249b
lzy <1679963578@qq.com>
2024-03-05 15:18:22 +0800

    feat: add border

--- src/components/Testnet.tsx
+++ src/components/Testnet.tsx
@@ -46,22 +46,37 @@ const Testnet = () => {
       <div
         data-aos="fade-up"
         data-aos-anchor-placement="top-bottom"
-        className="bg-[url(/prupleBg.png)] bg-cover object-cover bg-repeat h-[130px]  flex items-center justify-center "
+        className=" py-[1px]"
+        style={{
+          borderRadius: "1px",
+          backgroundImage:
+            "linear-gradient(to left, #000000 80%, transparent 1%), linear-gradient(to left, #000000 80%, transparent 10%), linear-gradient(to top, #000000 40%, transparent 10%), linear-gradient(to top, #000000 1%, transparent 1%)",
+          backgroundPosition: "left top, left bottom, left top, right top",
+          backgroundRepeat: "repeat-x, repeat-x, repeat-y, repeat-y",
+          backgroundSize: "30px 1px, 30px 1px, 30px 9px, 1px 9px",
+        }}
       >
-        <div className=" font-le text-2xl  text-white font-light  ">
-          <div className=" flex items-center">
-            If you are a BTC user, go to this
-            <span className="font-semibold ml-3"> Bridge page </span>
-            <div className=" mx-3">
-              <Icon name="arrowN" />
+        <div
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-bottom"
+          className="bg-[url(/prupleBg.png)] bg-cover object-cover bg-repeat h-[130px]  flex items-center justify-center "
+        >
+          <div className=" font-le text-2xl  text-white font-light  ">
+            <div className=" flex items-center">
+              If you are a BTC user, go to this
+              <span className="font-semibold ml-3"> Bridge page </span>
+              <div className=" mx-3">
+                <Icon name="arrowN" />
+              </div>
+              for testing out
             </div>
-            for testing out
-          </div>
-          <div className="flex justify-center ">
-            asset bridging between L1 and L2.
+            <div className="flex justify-center ">
+              asset bridging between L1 and L2.
+            </div>
           </div>
         </div>
       </div>
+
       <div
         data-aos="fade-up"
         data-aos-anchor-placement="top-bottom"
