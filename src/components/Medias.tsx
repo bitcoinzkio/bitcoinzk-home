4cd9757c1c3cbd4865d14e6564705f314f9f57ce
nonolook <looklooknono@outlook.com>
2025-09-19 14:06:50 +0800

    update medias

--- src/components/Medias.tsx
+++ src/components/Medias.tsx
@@ -1,8 +1,9 @@
 import { ReactNode } from "react";
-import { FaMedium, FaTelegram, FaXTwitter } from "react-icons/fa6";
-
+import { FaTelegram, FaXTwitter } from "react-icons/fa6";
+import { SiSubstack } from "react-icons/si";
 export const medias: { name: string, link: string, icon: ReactNode }[] = [
-    { name: 'Medium', icon: <FaMedium />, link: 'https://medium.com/@zyrabitcoinzk' },
+    // { name: 'Medium', icon: <FaMedium />, link: 'https://medium.com/@zyrabitcoinzk' },
+    { name: 'Substack', icon: <SiSubstack />, link: 'https://substack.com/@bitcoinzkzyra' },
     { name: 'X', icon: <FaXTwitter />, link: 'https://x.com/ZK_ZYRA' },
     { name: 'Telegram', icon: <FaTelegram />, link: 'https://t.me/BitcoinZK_Global' },
 ]
